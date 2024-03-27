c239cc20baae95b7f4b52d3aeb24707fce637d54
Hugo Barcelos <hfilipe12@hotmail.com>
2024-03-27 17:37:07 +0000

    add: solution of exercise003

--- Exercises/Exercise003.cs
+++ Exercises/Exercise003.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 namespace Exercises
 {
     public class Exercise003
     {
         // Replace the exception statement below with your code!
-        public string[] IceCreamFlavours => throw new NotImplementedException();
+        public string[] IceCreamFlavours = new string[] { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
 
-        // Replace the exception statement below with your code!
-        public int IceCreamCode(string iceCreamFlavour) => throw new NotImplementedException();
+
+        public string[] IceCreamCode()
+        {
+            return IceCreamFlavours;
+        }
+
+        public int IceCreamCode(string iceCreamFlavour)
+        {
+            for (int i = 0; i < IceCreamFlavours.Length; i++)
+            {
+                if (IceCreamFlavours[i] == iceCreamFlavour)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
     }
 }
